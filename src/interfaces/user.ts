c06ee4c3226c3c259851bc49c2606a50f1dd0585
crcascr <cristianj_20@hotmail.com>
2025-06-11 16:30:07 -0500

    feat(user): add user related interfaces

---
+++ src/interfaces/user.ts
@@ -0,0 +1,46 @@
+export interface Address {
+  street: string
+  suite: string
+  city: string
+  zipcode: string
+  geo: {
+    lat: string
+    lng: string
+  }
+}
+
+export interface Company {
+  name: string
+  catchPhrase: string
+  bs: string
+}
+
+export interface User {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: Address
+  phone: string
+  website: string
+  company: Company
+}
+
+export interface UserCardData {
+  id: number
+  name: string
+  email: string
+  avatar: string
+}
+
+export interface ApiResponse<T> {
+  data: T
+  status: number
+  message?: string
+}
+
+export interface ApiError {
+  message: string
+  status?: number
+  code?: string
+}
