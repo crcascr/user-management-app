1b2484d1272e99ec69cf12ca24114bd392f89b08
crcascr <cristianj_20@hotmail.com>
2025-06-11 16:28:29 -0500

    feat(global): add setup file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,51 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
 import App from './App.vue'
-import router from './router'
+import './styles/main.scss'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1976d2',
+          secondary: '#424242',
+          accent: '#82b1ff',
+          error: '#ff5252',
+          info: '#2196f3',
+          success: '#4caf50',
+          warning: '#ffc107',
+        },
+      },
+    },
+  },
+})
 
-app.use(router)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('./views/HomeView.vue'),
+    },
+  ],
+})
 
-app.mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
